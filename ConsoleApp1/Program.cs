907a8de9699cb302b24d0ee236f845cc20f3497f
Damian Raczek <draczekprojekt@gmail.com>
2024-11-26 10:19:34 +0100

    Application tests implemented

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Testy aplikacji konsolowej:");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.InstancjeKlasy}");
+
+            Osoba osoba1 = new Osoba();
+
+            Console.WriteLine("Podaj id osoby:");
+            int id = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine("Podaj imię osoby:");
+            string imie = Console.ReadLine() ?? "";
+            Osoba osoba2 = new Osoba(id, imie);
+
+            Osoba osoba3 = new Osoba(osoba2);
+
+            osoba1.Wypisz("Jan");
+            osoba2.Wypisz("Jan");
+            osoba3.Wypisz("Jan");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.InstancjeKlasy}");
         }
     }
 }
