97f84b2adaa991228a67cb6c678cb46fce8277e0
Damian Raczek <draczekprojekt@gmail.com>
2024-11-26 10:14:51 +0100

    Osoba class implemented

---
+++ ConsoleApp1/Osoba.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Osoba
+    {
+        public static int InstancjeKlasy = 0;
+        private int id;
+        private string imie;
+
+        public Osoba()
+        {
+            id = 0;
+            imie = "";
+            InstancjeKlasy++;
+        }
+
+        public Osoba(int id, string imie)
+        {
+            this.id = id;
+            this.imie = imie;
+            InstancjeKlasy++;
+        }
+
+        public Osoba(Osoba osoba)
+        {
+            id = osoba.id;
+            imie = osoba.imie;
+            InstancjeKlasy++;
+        }
+
+        public void Wypisz(string argument)
+        {
+            Console.WriteLine(string.IsNullOrEmpty(imie) == true ? "Brak danych" : $"Cześć {argument}, mam na imię {imie}");
+        }
+    }
+}
